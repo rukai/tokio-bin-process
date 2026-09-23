479ac4baa46846cfcf7c819b5257044ad7467698
Lucas Kent <lucas.kent@swarmfarm.com>
2026-09-23 12:16:29 +1000

    fix formatter

--- cooldb/src/tracing_panic_handler.rs
+++ cooldb/src/tracing_panic_handler.rs
@@ -40,9 +40,10 @@ impl fmt::Display for BacktraceFormatter {
         let mut print_path = move |fmt: &mut fmt::Formatter<'_>, path: BytesOrWideString<'_>| {
             let path = path.into_path_buf();
             if let Ok(cwd) = &cwd
-                && let Ok(suffix) = path.strip_prefix(cwd) {
-                    return fmt::Display::fmt(&suffix.display(), fmt);
-                }
+                && let Ok(suffix) = path.strip_prefix(cwd)
+            {
+                return fmt::Display::fmt(&suffix.display(), fmt);
+            }
             fmt::Display::fmt(&path.display(), fmt)
         };
 
